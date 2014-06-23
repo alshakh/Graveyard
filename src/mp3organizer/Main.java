d576eabcd2c3f91436a3f8f877d55fdd90e149ad
alshakh <ahmed.s.alshakh@gmail.com>
2014-06-23 15:39:11 -0600

    init

---
+++ src/mp3organizer/Main.java
@@ -0,0 +1,16 @@
+package mp3organizer;
+
+/**
+ *
+ * @author Ahmed Alshakh <ahmed.s.alshakh@gmail.com>
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
