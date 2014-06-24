8f09d20261ed315d41789dd4739315882fa5e9a3
alshakh <ahmed.s.alshakh@gmail.com>
2014-06-24 12:07:13 -0600

    Added : clean function and documentation for SortPattern

--- src/mp3organizer/core/SortPattern.java
+++ src/mp3organizer/core/SortPattern.java
@@ -1,51 +1,69 @@
-
 package mp3organizer.core;
 
 import java.util.EnumMap;
 import org.jaudiotagger.tag.FieldKey;
 
-
 /**
- * 
+ * Handles the patterns for sorting the files.
+ * It cannot be changed after instantiating. to change the pattern create new
+ * object
  * @author Ahmed Alshakh <ahmed.s.alshakh@gmail.com>
  */
 class SortPattern {
+
+    /**
+     * handles the pattern in a String object
+     */
+    private String patternStr;
+
     /**
      * Aliases for fields
      */
-    public static final EnumMap<FieldKey,String> ALIASES = 
-            new EnumMap<FieldKey,String>(FieldKey.class);
+    public static final EnumMap<FieldKey, String> ALIASES
+            = new EnumMap<FieldKey, String>(FieldKey.class);
+
     static {
-            ALIASES.put(FieldKey.ARTIST, "<artist>");
-            ALIASES.put(FieldKey.YEAR, "<year>");
-            ALIASES.put(FieldKey.ALBUM, "<album>");
-            ALIASES.put(FieldKey.TITLE, "<title>");
+        ALIASES.put(FieldKey.ARTIST, "<artist>");
+        ALIASES.put(FieldKey.YEAR, "<year>");
+        ALIASES.put(FieldKey.ALBUM, "<album>");
+        ALIASES.put(FieldKey.TITLE, "<title>");
     }
-   
-    
+
     /**
-     * 
-     * @param patternStr 
+     * Constructor takes pattern in string to instantiate the object.
+     * @param patternStr
      */
-    public SortPattern(String patternStr){
+    public SortPattern(String patternStr) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-    
+
     /**
-     * process original path to new path
+     * process original path to new path,
+     *
      * @param file original file to get the original path/tag from
-     * @return 
+     * @return
      */
-    public String proccessFilePath(MediaFile file){
+    public String proccessFilePath(MediaFile file) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-    
+
     /**
-     * check if sort pattern is valid
+     * cleans trailing spaces and double slashes.
+     */
+    public void clean() {
+        throw new UnsupportedOperationException("Not supported yet.");
+//        if (!isValid(patternStr)) {
+//            return;
+//        }
+    }
+
+    /**
+     * check if sort pattern is valid.
+     *
      * @param patternStr
      * @return true if valid pattern, false otherwise
      */
-    public static boolean isValid(String patternStr){
+    public static boolean isValid(String patternStr) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 }
