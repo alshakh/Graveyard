9f5f71e64414b170cbecd81c3ea2a2a789d46980
alshakh <ahmed.s.alshakh@gmail.com>
2014-06-24 19:51:05 -0600

    Created: test for basic file operations

---
+++ test/mp3organizer/core/FileOperations.java
@@ -0,0 +1,41 @@
+
+package mp3organizer.core;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Test if can work with files. copy, delete, move.
+ * @author Ahmed Alshakh <ahmed.s.alshakh@gmail.com>
+ */
+public class FileOperations {
+    
+    public FileOperations() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
