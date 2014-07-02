97294ab7a6757ceacc110303035a57fd53ac55a1
alshakh <ahmed.s.alshakh@gmail.com>
2014-07-02 16:12:03 -0600

    Fixed fileChooser, Add and remove buttons

--- src/mp3organizer/gui/FileFrame.java
+++ src/mp3organizer/gui/FileFrame.java
@@ -1,15 +1,16 @@
 package mp3organizer.gui;
-import javax.swing.JFileChooser;
+
 import java.io.File;
 import javax.swing.DefaultListModel;
-import javax.swing.JList;
+import javax.swing.JFileChooser;
+import mp3organizer.core.MediaFile;
+import mp3organizer.core.MediaFileList;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author yousef-alsber
@@ -42,7 +43,7 @@ public class FileFrame extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        addButtton.setText("Add");
+        addButtton.setText("+ file ...");
         addButtton.setToolTipText("");
         addButtton.setActionCommand("Brow");
         addButtton.setName("addButtton"); // NOI18N
@@ -104,14 +105,14 @@ public class FileFrame extends javax.swing.JFrame {
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 118, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(28, 28, 28)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addGap(36, 36, 36)
                         .addComponent(addButtton)
-                        .addGap(18, 18, 18)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(removeButtton)))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -124,24 +125,20 @@ public class FileFrame extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void addButttonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addButttonActionPerformed
-        File [] mp3Files;
-//        JList jlist;
-        DefaultListModel model = new DefaultListModel();
-        JList list = new JList(model);
         JFileChooser fc = new JFileChooser();
         fc.setMultiSelectionEnabled(true);
-        fc.showOpenDialog(this);
-        mp3Files = fc.getSelectedFiles();
-//        jlist = new JList(mp3Files);
-        for(int i=0; i<mp3Files.length; i++){
-            System.out.print(mp3Files[i].getName());
-            model.addElement(mp3Files[i].getName());
+        int ret = fc.showOpenDialog(this);
+        if (ret == JFileChooser.APPROVE_OPTION) {
+            DefaultListModel listModel = getNewListModel();
+            File[] files = fc.getSelectedFiles();
+            for (File f : files) {
+                listModel.addElement(f.getAbsolutePath());
+            }
+            songList.setModel(listModel);
         }
-        //songList.setModel(jlist);
     }//GEN-LAST:event_addButttonActionPerformed
 
     private void changeButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_changeButtonActionPerformed
-       
         TagesFrame tagesFrame = new TagesFrame();
         tagesFrame.setVisible(true);
         this.setVisible(false);
@@ -156,9 +153,37 @@ public class FileFrame extends javax.swing.JFrame {
     }//GEN-LAST:event_sortButtonActionPerformed
 
     private void removeButttonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_removeButttonActionPerformed
-        // TODO add your handling code here:
+        int[] selectedIndices = songList.getSelectedIndices();
+        DefaultListModel modelList = getNewListModel();
+        if (selectedIndices.length == 0) {
+            return;
+        }
+        for (int i = selectedIndices.length - 1; i >= 0; i--) {
+            modelList.remove(selectedIndices[i]);
+        }
+        songList.setModel(modelList);
     }//GEN-LAST:event_removeButttonActionPerformed
 
+    private MediaFileList getMediaFileList() {
+        MediaFileList mediaFileList = new MediaFileList();
+        for (int i = 0 ;i< songList.getModel().getSize() ; i++) {
+            try {
+                mediaFileList.addFile(new MediaFile((String)songList.getModel().getElementAt(i)));
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+                //TODO do something to tell the user
+            }
+        }
+        return mediaFileList;
+    }
+    //
+    private DefaultListModel getNewListModel() {
+        DefaultListModel listModel = new DefaultListModel();
+        for (int i = 0; i < songList.getModel().getSize(); i++) {
+            listModel.addElement(songList.getModel().getElementAt(i));
+        }
+        return listModel;
+    }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton addButtton;
     private javax.swing.JButton changeButton;
