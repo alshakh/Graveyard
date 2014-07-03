18b27e87f943622445a836c20e5f719f2d329cce
alshakh <ahmed.s.alshakh@gmail.com>
2014-07-03 01:19:07 -0600

    merged with gui testing

--- test/mp3organizer/gui/TagsFrameTest.java
+++ test/mp3organizer/gui/TagsFrameTest.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package mp3organizer.gui;
 
@@ -10,8 +6,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.fest.swing.fixture.FrameFixture;
-import org.fest.swing.core.GenericTypeMatcher;
-import javax.swing.JButton;
 
 /**
  *
